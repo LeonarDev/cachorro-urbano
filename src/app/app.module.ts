6cc828a7253389ac86b0b8af898d823a46eade50
LeonarDev <leonardo.majevski@gmail.com>
2022-07-15 23:41:10 -0300

    05 - Implementa rest api fake

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +15,8 @@ import { RodapeComponent } from './rodape/rodape.component';
     RodapeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
